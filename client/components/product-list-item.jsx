e0aeb0584197db1c42b79931be06582c979b4a64
saminsoolee <samuelminsoolee71@gmail.com>
2020-10-14 22:43:02 +0000

    Completed Styling for Frontend

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 class ProductListItem extends React.Component {
   render() {
     return (
-      <div className="col-md-4 shadow-sm">
-        <div className="card p-1">
-          <img className="card-img-top img-size text-center p-1 m-1" src={this.props.card.image.toString()}></img>
+      <div className="col-md-3 shadow-sm m-4 card-container">
+        <div className="card p-1 border-0">
+          <img className="card-img-top mb-0 image-container" src={this.props.card.image.toString()}></img>
         </div>
         <div className="card-body">
           <h4>{this.props.card.name}</h4>
