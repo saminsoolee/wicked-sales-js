e4c61001dd7f2b7b59eeb95756aa32d18cc077dc
saminsoolee <samuelminsoolee71@gmail.com>
2020-10-16 23:12:28 +0000

    Initial state to include product and render to null

---
+++ client/components/product-details.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+class ProductDetails extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      product: null
+    };
+  }
+}
+render(){
+  return null;
+}
+export default ProductDetails;
